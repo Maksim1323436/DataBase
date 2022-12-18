014394e7c5fd8a1314cdc82c62fe647eccdb3970
Andrey <andrey2004182@gmail.com>
2022-12-18 22:01:01 +0300

    Добавьте файлы проекта.

---
+++ WindowsAPI/DataBaseEditor/FormEditorTable1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataBaseLib;
+
+namespace WindowsFormsApp1.DataBaseEditor
+{
+    public partial class FormEditorTable1 : Form
+    {
+        public FormEditorTable1()
+        {
+            InitializeComponent();
+        }
+        string _tableName = "Клиент";
+
+        private void Table1_Load(object sender, EventArgs e)
+        {
+            DataBaseCommadsManager manager = new DataBaseCommadsManager();
+            dataGridView1.DataSource = manager.GetDataTable(_tableName);
+        }
+
+        private void BtnInsert_Click(object sender, EventArgs e)
+        {
+            string[] args = new string[4];
+            args[0] = textBox2.Text;
+            args[1] = textBox3.Text;
+            args[2] = textBox4.Text;
+            args[3] = textBox1.Text;
+
+
+            DataBaseCommadsManager manager = new DataBaseCommadsManager();
+            manager.Insert(args, _tableName);
+            manager.GetDataTable(_tableName);
+            dataGridView1.DataSource = manager.GetDataTable(_tableName);
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+
+            int currentRow = dataGridView1.CurrentCell.RowIndex;
+            string[] args = new string[1];
+            args[0] = dataGridView1[0, currentRow].Value.ToString();
+
+            DataBaseCommadsManager manager = new DataBaseCommadsManager();
+            manager.Delete(args, _tableName);
+            dataGridView1.DataSource = manager.GetDataTable(_tableName);
+        }
+
+        private void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            // дома
+        }
+    }
+}
