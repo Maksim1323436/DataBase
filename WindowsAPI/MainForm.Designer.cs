3de84e198649a53ffc1cd63e0ce413b286260111
Maksim1323436 <113716548+Maksim1323436@users.noreply.github.com>
2022-12-20 13:21:40 +0300

    Update MainForm.Designer.cs

--- WindowsAPI/MainForm.Designer.cs
+++ WindowsAPI/MainForm.Designer.cs
@@ -2,7 +2,7 @@
 {
     partial class MainForm
     {
-        /// <summary>
+        /// <sumary>
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
